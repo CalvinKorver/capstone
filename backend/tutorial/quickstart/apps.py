0061c48729990e187abc0676a1993c244ae7b499
kyle-mcnulty <kylemcnulty@gmail.com>
2018-02-20 16:07:58 -0800

    added auth for backend, need to revisit structure for our users

--- backend/tutorial/quickstart/apps.py
+++ backend/tutorial/quickstart/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class QuickstartConfig(AppConfig):
     name = 'quickstart'
+
+    def ready(self):
+        from . import signals
--- backend/tutorial/urls.py
+++ backend/tutorial/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from tutorial.quickstart import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+# urlpatterns += [ url(r'^obtain-auth-token/$', obtain_auth_token) ]
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -11,5 +14,6 @@ router.register(r'clients', views.ClientViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^obtain-auth-token/$', obtain_auth_token),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
