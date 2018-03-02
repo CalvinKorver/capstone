745d55eae30cfddfa0b8d623d20b075afe6b0d08
Calvin <calvin.korver@gmail.com>
2018-03-02 14:01:15 -0800

    Configure router and login page before merge

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { login } from './util/Auth.js'
+import './util/ApiClient.js'
 
 class App extends Component {
+
+  constructor() {
+    super();
+  }
+
+  handleLogin(e) {
+    e.preventDefault();
+    console.log("clicked");
+    login("calvin", "korver");
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +26,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <form>
+          First name:<br/>
+          <input type="text" name="firstname"/>
+            <br/>
+          Last name:<br/>
+          <input type="text" name="lastname"/>
+          <button onClick={e => this.handleLogin(e)}>submit</button>
+        </form>
+        
       </div>
     );
   }
---
+++ frontend/frontend/src/actions/index.js
@@ -0,0 +1,9 @@
+// file: src/actions/index.js
+import * as actionType from './types';
+
+export const setToken = (data) => {
+  return {
+    type: actionType.SET_TOKEN,
+    data
+  }
+}
