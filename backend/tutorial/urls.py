90af74698bbf4a56bd3e4d9c9e00ac9a3f23ef4f
kyle-mcnulty <kylemcnulty@gmail.com>
2018-03-04 15:52:39 -0800

    fixed urls

--- backend/tutorial/urls.py
+++ backend/tutorial/urls.py
@@ -14,7 +14,8 @@ router.register(r'clients', views.ClientViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include('rest_auth.urls')),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     # url(r'^registration/', include('rest_auth.registration.urls')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
