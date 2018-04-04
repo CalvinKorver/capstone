ca43f90e5ceca6dce07f6258a992ee107d26ed7e
kyle-mcnulty <kylemcnulty@gmail.com>
2018-04-03 17:31:52 -0700

    working on views and bash script

--- backend/tutorial/quickstart/views.py
+++ backend/tutorial/quickstart/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User, Group
 from tutorial.quickstart.models import Client, Event_Type, Client_Type, Status, Auth_User_Type, Case_Type, Case, Auth_User_Case, Event, Case_Event
 from rest_framework import viewsets
+# from rest_framework.decorators import action
+from rest_framework.decorators import detail_route, list_route
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer, ClientSerializer, EventTypeSerializer, ClientTypeSerializer, StatusSerializer, AuthUserTypeSerializer, CaseTypeSerializer, CaseSerializer, AuthUserCaseSerializer, EventSerializer, CaseEventSerializer
 
 
@@ -67,6 +69,25 @@ class CaseViewSet(viewsets.ModelViewSet):
     """
     queryset = Case.objects.all()
     serializer_class = CaseSerializer
+
+    @list_route(methods=['post'])#, detail=True)
+    def new_case(self, request, pk=None):
+        case_name = request.data.name
+        case_type = Case_Type.objects.get(name=request.data.case_type)
+        client = Client.objects.get(first_name=request.data.first_name, last_name=request.data.last_name)
+        case = self.get_object()
+        case.case_type = case_type.id
+        case.client = client.id
+        case.save()
+        return Response({'status': 'request handled'})
+        # serializer = PasswordSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     user.set_password(serializer.data['password'])
+        #     user.save()
+        #     return Response({'status': 'password set'})
+        # else:
+        #     return Response(serializer.errors,
+        #                     status=status.HTTP_400_BAD_REQUEST)
 
 class AuthUserCaseViewSet(viewsets.ModelViewSet):
     """
